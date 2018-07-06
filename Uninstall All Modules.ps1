693ec878fad4f22f8f40be31c57d644d480b4dd5
Dennis Moon <dmoon396@hotmail.com>
2018-07-06 09:57:32 -0500

    Added Uninstall All Modules script

---
+++ Uninstall All Modules.ps1
@@ -0,0 +1,40 @@
+﻿# Get list of installed versions of AzureRM
+# Get-Module -Name AzureRM -ListAvailable | Select-Object -Property Name,Version,Path
+
+# Function that uninstalls a specific version of a module 
+# and all of its dependencies
+function Uninstall-AllModules {
+  param(
+    [Parameter(Mandatory=$true)]
+    [string]$TargetModule,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Version,
+
+    [switch]$Force
+  )
+
+  $AllModules = @()
+
+  'Creating list of dependencies...'
+  $target = Find-Module $TargetModule -RequiredVersion $version
+  $target.Dependencies | ForEach-Object {
+    $AllModules += New-Object -TypeName psobject -Property @{name=$_.name; version=$_.requiredversion}
+  }
+  $AllModules += New-Object -TypeName psobject -Property @{name=$TargetModule; version=$Version}
+
+  foreach ($module in $AllModules) {
+    Write-Host ('Uninstalling {0} version {1}' -f $module.name,$module.version)
+    try {
+      Uninstall-Module -Name $module.name -RequiredVersion $module.version -Force:$Force -ErrorAction Stop
+    } catch {
+      Write-Host ("`t" + $_.Exception.Message)
+    }
+  }
+}
+
+# Runs the uninstall
+Uninstall-AllModules -TargetModule AzureRM -Version 4.3.1 -Force
+
+
+# Install-Module -Name AzureRM
